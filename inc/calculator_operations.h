c789b3707c1ac47eb3b409bc2a7c6632e332b2ad
99002636 <71487079+99002636@users.noreply.github.com>
2020-09-19 09:55:17 +0530

    Update calculator_operations.h

--- inc/calculator_operations.h
+++ inc/calculator_operations.h
@@ -8,6 +8,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 #include <stdio_ext.h>
 
 /**
@@ -43,5 +44,15 @@ int multiply(int operand1, int operand2);
 * @note returns 0 for divide by 0 error
 */
 int divide(int operand1, int operand2);
+
+/**
+*does the power operation of operand1 by operand2 and returns the result
+* @param[in] operand1
+* @param[in] operand2 
+* @return integer value of the operand1 / operand2
+* @note returns 1 for power by 0 
+*/
+
+int power(int operand1,int operand2);
 
 #endif  /* #define __CALCULATOR_OPERATIONS_H__ */
